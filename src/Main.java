b5f387395e9138bcff7a29531cd4b70f2ce61449
Dyllon <dyllon.g@outlook.com>
2023-03-30 15:49:11 +0800

    Create input handler

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
 package src;
 
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
 
-        // ingest data from data/
+        System.out.println("=== Singapore Weather Query System ===");
+
+        // ask for and receive input (matric num)
+        System.out.print("Enter the matriculation number as query: ");
+        String matricNum = scan.nextLine();
+
+        // process input
+        Map<String, Object> query = Query.parseQueryInput(matricNum);
+
+        System.out.println(query);
+
+        // ingest data from data/*.csv (data is pre-sorted)
+
+        // scan data
+
+        // output result to file
     }
 }
